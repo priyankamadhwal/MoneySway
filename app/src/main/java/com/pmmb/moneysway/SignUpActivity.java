eed8255883f6d183e917b0a85af900638dd5a19f
Priyanka Madhwal <priyanka0701m@gmail.com>
2019-11-02 21:48:17 +0530

    Creating Sign up and login activities

--- app/src/main/java/com/pmmb/moneysway/SignUpActivity.java
+++ app/src/main/java/com/pmmb/moneysway/SignUpActivity.java
@@ -6,15 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
-    public void onClickSignUpNowButton(View view) {
-        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+    public void onClickSignInNowButton(View view) {
+        startActivity(new Intent(SignUpActivity.this, MainActivity.class));
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
     }
 }
