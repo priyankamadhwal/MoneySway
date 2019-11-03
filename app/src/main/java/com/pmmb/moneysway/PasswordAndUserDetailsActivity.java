bf612a4169fc0622a4041265aa769d811740d571
Priyanka Madhwal <priyanka0701m@gmail.com>
2019-11-03 18:48:26 +0530

    Adding firebase auth: phone number + password (sign up/ sign in)

---
+++ app/src/main/java/com/pmmb/moneysway/PasswordAndUserDetailsActivity.java
@@ -0,0 +1,73 @@
+package com.pmmb.moneysway;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+
+
+public class PasswordAndUserDetailsActivity extends AppCompatActivity {
+
+    private EditText signUpUserNameEditText, signUpPasswordEditText;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_and_user_details);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signUpUserNameEditText = findViewById(R.id.signUpUserNameEditText);
+        signUpPasswordEditText = findViewById(R.id.signUpPasswordEditText);
+
+    }
+
+    public void onClickDoneButton(View view) {
+        String phoneNumber, name, password;
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            phoneNumber = getIntent().getExtras().getString("phoneNumber");
+            name = signUpUserNameEditText.getText().toString();
+            password = signUpPasswordEditText.getText().toString();
+            if (name.length()<4) {
+                signUpUserNameEditText.setError("Enter name (atleast 6 characters)");
+            }
+            if (password.length()<4) {
+                signUpPasswordEditText.setError("Enter password (atleast 3 characters)");
+            }
+            if (name.length()>3 && password.length()>6) {
+                mAuth.createUserWithEmailAndPassword(phoneNumber + "@" + getResources().getString(R.string.app_name).toLowerCase() + ".com", password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(), "Sign up successful!", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(PasswordAndUserDetailsActivity.this, HomeActivity.class);
+                                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                    startActivity(intent);
+                                }
+                                else {
+                                    FirebaseAuthException e = (FirebaseAuthException)task.getException();
+                                    Log.e("SignUp", "Failed Registration", e);
+                                    Toast.makeText(getApplicationContext(), "Sign up failed! Please try again later", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        }
+        else {
+            Toast.makeText(this, "Something went wrong! Please try again later", Toast.LENGTH_LONG).show();
+        }
+    }
+}
--- app/src/main/java/com/pmmb/moneysway/SignUpActivity.java
+++ app/src/main/java/com/pmmb/moneysway/SignUpActivity.java
@@ -1,20 +1,140 @@
 package com.pmmb.moneysway;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.concurrent.TimeUnit;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
 
 public class SignUpActivity extends AppCompatActivity {
 
-    public void onClickSignInNowButton(View view) {
-        startActivity(new Intent(SignUpActivity.this, MainActivity.class));
-    }
+    private TextView signUpCountryCodeTextView;
+    private EditText signUpPhoneNumberEditText, signUpOtpEditText;
+    private Button signUpButton, signUpVerifyButton;
+    private FirebaseAuth mAuth;
+    private String mVerificationId;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signUpCountryCodeTextView = findViewById(R.id.signUpCountryCodeTextView);
+        signUpPhoneNumberEditText = findViewById(R.id.signUpPhoneNumberEditText);
+        signUpOtpEditText = findViewById(R.id.signUpOtpEditText);
+        signUpButton = findViewById(R.id.signUpButton);
+        signUpVerifyButton = findViewById(R.id.signUpVerifyButton);
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
+                String code = credential.getSmsCode();
+                if (code != null) {
+                    signUpOtpEditText.setText(code);
+                }
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Toast.makeText(SignUpActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                Toast.makeText(SignUpActivity.this, "OTP has been sent!", Toast.LENGTH_SHORT).show();
+                signUpPhoneNumberEditText.setEnabled(false);
+                signUpButton.setVisibility(View.INVISIBLE);
+                signUpVerifyButton.setVisibility(View.VISIBLE);
+                signUpOtpEditText.setVisibility(View.VISIBLE);
+                signUpOtpEditText.requestFocus();
+                mVerificationId = s;
+            }
+        };
     }
+
+    public void onClickSignInNowButton(View view) {
+        startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+    }
+
+    public void onClickSignUpButton(View view) {
+        String phoneNumber = signUpPhoneNumberEditText.getText().toString();
+        if(phoneNumber.length() != 10){
+            Toast.makeText(SignUpActivity.this, "Invalid Phone Number Format!", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            String countryCode = signUpCountryCodeTextView.getText().toString();
+            PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                    countryCode + phoneNumber,            // Phone number to verify
+                    60,                                   // Timeout duration
+                    TimeUnit.SECONDS,                        // Unit of timeout
+                    this,                             // Activity (for callback binding)
+                    mCallbacks);                             // OnVerificationStateChangedCallbacks
+        }
+    }
+
+    public void onClickSignUpVerifyButton(View view) {
+        String code = signUpOtpEditText.getText().toString().trim();
+        if (code.isEmpty() || code.length() < 6) {
+            signUpOtpEditText.setError("Enter valid code");
+            signUpOtpEditText.requestFocus();
+            return;
+        }
+        verifyVerificationCode(code);
+    }
+
+    private void verifyVerificationCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, code);
+        credential.getSmsCode();
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignUpActivity.this, "Verification successful!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(SignUpActivity.this, PasswordAndUserDetailsActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            intent.putExtra("phoneNumber", signUpPhoneNumberEditText.getText().toString());
+                            startActivity(intent);
+
+                        } else {
+
+                            String message = "Verification unsuccessful! Something is wrong, we will fix it soon.";
+
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                message = "Verification unsuccessful! Invalid code entered.";
+                            }
+
+                            Toast.makeText(SignUpActivity.this, message, Toast.LENGTH_LONG).show();
+
+
+                        }
+                    }
+                });
+    }
+
+
 }
